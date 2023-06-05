f84e37743b8a2849f1a8edf6b7b0b5d3da57141b
AlexisChasi <alexis.chasi@epn.edu.ec>
2023-06-05 16:32:21 -0500

    hasta rutas

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path');
+const { engine }  = require('express-handlebars')
 
 // Inicializaciones
 const app = express()
@@ -7,6 +8,15 @@ const app = express()
 // Configuraciones 
 app.set('port',process.env.port || 3000) // toma el puerto donde se despliega la app o sino en el puerto determinado 3000
 app.set('views',path.join(__dirname, 'views'))
+
+app.set('views',path.join(__dirname, 'views'))
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname:'.hbs'
+}))
+app.set('view engine','.hbs')
 
 // Middlewares 
 app.use(express.urlencoded({extended:false}))
@@ -16,8 +26,9 @@ app.use(express.urlencoded({extended:false}))
 
 // Rutas 
 app.get('/',(req,res)=>{
-    res.send("Server ok!")
+    res.render('index')
 })
+app.use(require('./routers/index.routes'))
 
 // Archivos estáticos
 app.use(express.static(path.join(__dirname,'public')))
